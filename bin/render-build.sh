fc57e024a37c000e61f0294b36aa6df4bbcc6c10
Juanequex <ramon.g.uz37@gmail.com>
2023-12-30 20:15:31 -0600

    new way to run rails task in each deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,7 @@ set -o errexit
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
+./bin/rails railties:install:migrations
 ./bin/rails db:migrate
 ./bin/rails db:seed
-rake spree_sample:load
+# rake spree_sample:load
